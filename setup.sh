ea30305564d707c05e3f10884eb2b9adfa7665a7
Andrew LaCharite <adlacharite@gmail.com>
2020-03-23 00:56:45 +0000

    add more detail to emacs install

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-# wget http://ftp.gnu.org/gnu/emacs/emacs-26.3.tar.gz
-# tar -xvzf emacs*
+wget http://ftp.gnu.org/gnu/emacs/emacs-26.3.tar.gz
+tar -xvzf emacs*
 git config --global user.name "Andrew LaCharite"
 git config --global user.email adlacharite@gmail.com
 apt install emacs
+apt install gcc make libncurses5-dev
+./configure --with-gnutls=no
+make
+make install
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
